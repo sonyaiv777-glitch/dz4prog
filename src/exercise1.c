fe9bed7c64216e7735e3b1ae8d336a3862c982af
Иванова Софья Анатольевна ИП-511 <sonyaiv777@gmail.com>
2025-10-09 23:12:26 +0700

    Исправление задания 1.3

--- src/exercise1.c
+++ src/exercise1.c
@@ -3,10 +3,10 @@
 int main(int argc,char** argv) {
     double num[10];
     double sum = 0;
-    scanf("%f %f %f %f %f %f %f %f %f %f", &num[0], &num[1], &num[2], &num[3], &num[4], &num[5], &num[6], &num[7], &num[8], &num[9]);
+    scanf("%lf %lf %lf %lf %lf %lf %lf %lf %lf %lf", &num[0], &num[1], &num[2], &num[3], &num[4], &num[5], &num[6], &num[7], &num[8], &num[9]);
     for (int i = 0; i < 10; i++) {
         sum += num[i];
     }
-    printf("%.2f", sum / (double)10);
+    printf("%.2lf", sum / (double)10);
     return 0;
 }
