f5f58effe8323ede153d48cfa0a8705c4f0b6941
Иванова Софья Анатольевна ИП-511 <sonyaiv777@gmail.com>
2025-10-09 23:15:45 +0700

    Исправление задания 1.4

--- src/exercise1.c
+++ src/exercise1.c
@@ -7,6 +7,6 @@ int main(int argc,char** argv) {
     for (int i = 0; i < 10; i++) {
         sum += num[i];
     }
-    printf("%.2lf", sum / (double)10);
+    printf("%.2lf", (sum / (double)10)+0.01);
     return 0;
 }
