fcae590bd33c7481221275fc5b9ad358049e079d
Иванова Софья Анатольевна ИП-511 <sonyaiv777@gmail.com>
2025-10-14 19:12:23 +0700

    Исправление задания 1.12

--- src/exercise3.c
+++ src/exercise3.c
@@ -1,20 +1,22 @@
+#include <stdlib.h>
 #include <stdio.h>
-
-int main(int argc,char** argv) {
-    float num[10];
-    scanf("%f %f %f %f %f %f %f %f %f %f", &num[0], &num[1], &num[2], &num[3], &num[4], &num[5], &num[6], &num[7], &num[8], &num[9]);
-    int K = 2;
-    
-    if (K < 0) {
-        K = K + 10;
+int main(int argc,char** argv){
+    int k, i;
+    float a[10];
+    float b[10];
+    for(i = 0; i < 10; i++){
+        scanf("%f", &a[i]);
     }
-    float print[10];
-    for (int i = 0; i < 10; i++) {
-        print[(i + K) % 10] = num[i];
+    scanf("%d", &k);
+    k = k%10;
+    if(k<0)
+        k=10+k;
+    for(i = 0; i < 10; i++){
+        b[(i+k)%10] = a[i];
     }
-
-    for (int i = 0; i < 10; i++) {
-        printf("%.3f ", print[i]);
+    for(i = 0; i < 10; i++){
+        printf("%.2lf", b[i]);
     }
+    printf("\n");
     return 0;
 }
