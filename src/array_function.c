e32d90975e927c244491ee902d2be8e0dc6789cc
Иванова Софья Анатольевна ИП-511 <sonyaiv777@gmail.com>
2025-10-15 21:57:07 +0700

    Выполнено задание 1

--- src/array_function.c
+++ src/array_function.c
@@ -1,6 +1,14 @@
 #include "../include/array_function.h"
 
-void join_int_arrays(int *src1, size_t size_src1, int *src2, size_t size_src2, int *dest) {}
+void join_int_arrays(int *src1, size_t size_src1, int *src2, size_t size_src2, int *dest) {
+    int dest_index = 0;
+    for (int i = 0; i < size_src1; i++ ) {
+        dest[dest_index++] = src1[i];
+    }
+    for (int i = 0; i < size_src2; i++ ) {
+        dest[dest_index++] = src2[i];
+    }
+}
 
 void join_and_sort_int_arrays(int* src1, size_t size_src1, int* src2, size_t size_src2, int* dest) {}
 
