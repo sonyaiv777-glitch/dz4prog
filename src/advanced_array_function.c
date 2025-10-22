e22f6a3e3bb92bad7482463c5947598f08893c66
Иванова Софья Анатольевна ИП-511 <sonyaiv777@gmail.com>
2025-10-22 21:49:02 +0700

    Выполнено задание 2

--- src/advanced_array_function.c
+++ src/advanced_array_function.c
@@ -24,6 +24,26 @@ int max_subarray_sum(int* nums, int size) {
     return maxs;
 }
 
+int length_of_lis(int* nums, int numsSize) {
+    int len = 1;
+    int max_len = 1;
+    for (int i = 0; i < numsSize - 1; i++) {
+        if (nums[i] <= nums[i + 1]) {
+            len++;
+        } else {
+            if (len > max_len) {
+                max_len = len;
+            }
+            len = 1;
+        }
+    }
+
+    if (len > max_len) {
+        max_len = len;
+    }
+    
+    return max_len;
+}
 
 
 
